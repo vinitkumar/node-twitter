9d5e2319ca2f2388d594c2976d44dabf683686ba
Vinit Kumar <vinitcool76@gmail.com>
2013-05-22 17:06:33 +0530

    more fixes

--- app/models/user.js
+++ app/models/user.js
@@ -5,7 +5,8 @@
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema
   , crypto = require('crypto')
-  , _ = require('underscore');
+  , _ = require('underscore')
+  , authTypes = ['twitter', 'facebook']
 
 
 
--- config/passport.js
+++ config/passport.js
@@ -1,17 +1,46 @@
-var mongoose = require('mongoose'),
-    FacebookStrategy = require('passport-facebook').Strategy,
-    GithubStrategy = require('passport-github').Strategy,
-    User = mongoose.model('User');
+
+var mongoose = require('mongoose')
+  , LocalStrategy = require('passport-local').Strategy
+  , TwitterStrategy = require('passport-twitter').Strategy
+  , FacebookStrategy = require('passport-facebook').Strategy
+  , GitHubStrategy = require('passport-github').Strategy
+  , GoogleStrategy = require('passport-google-oauth').Strategy
+  , User = mongoose.model('User')
 
 
 module.exports = function (passport, config) {
+  // require('./initializer')
 
-  //serialize sessions
+  // serialize sessions
   passport.serializeUser(function(user, done) {
+    done(null, user.id)
+  })
+
+  passport.deserializeUser(function(id, done) {
     User.findOne({ _id: id }, function (err, user) {
-      done(err, user);
-    });
-  });
+      done(err, user)
+    })
+  })
+
+  // use local strategy
+  passport.use(new LocalStrategy({
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    function(email, password, done) {
+      User.findOne({ email: email }, function (err, user) {
+        if (err) { return done(err) }
+        if (!user) {
+          return done(null, false, { message: 'Unknown user' })
+        }
+        if (!user.authenticate(password)) {
+          return done(null, false, { message: 'Invalid password' })
+        }
+        return done(null, user)
+      })
+    }
+  ))
+
 
   // use facebook strategy
   passport.use(new FacebookStrategy({
@@ -21,7 +50,7 @@ module.exports = function (passport, config) {
     },
     function(accessToken, refreshToken, profile, done) {
       User.findOne({ 'facebook.id': profile.id }, function (err, user) {
-        if (err) { return done(err); }
+        if (err) { return done(err) }
         if (!user) {
           user = new User({
               name: profile.displayName
@@ -29,18 +58,18 @@ module.exports = function (passport, config) {
             , username: profile.username
             , provider: 'facebook'
             , facebook: profile._json
-          });
+          })
           user.save(function (err) {
-            if (err) console.log(err);
-            return done(err, user);
-          });
+            if (err) console.log(err)
+            return done(err, user)
+          })
         }
         else {
-          return done(err, user);
+          return done(err, user)
         }
-      });
+      })
     }
-  ));
+  ))
 
   // use github strategy
   passport.use(new GitHubStrategy({
@@ -57,16 +86,17 @@ module.exports = function (passport, config) {
             , username: profile.username
             , provider: 'github'
             , github: profile._json
-          });
+          })
           user.save(function (err) {
-            if (err) console.log(err);
-            return done(err, user);
-          });
+            if (err) console.log(err)
+            return done(err, user)
+          })
         } else {
-          return done(err, user);
+          return done(err, user)
         }
-      });
+      })
     }
-  ));
+  ))
 
-};
+
+}
