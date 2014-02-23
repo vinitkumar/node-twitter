27a271b75e31ec06bbba2bb8c99674d7ae49ff27
vinit Kumar <vinit.kumar@changer.nl>
2014-02-23 09:26:15 +0530

    add twitter login and update fb app settings

--- config/config.js
+++ config/config.js
@@ -8,15 +8,20 @@ module.exports = {
       name: 'Node Twitter'
     },
     facebook: {
-      clientID: "680920985255957",
-      clientSecret: "2d5c19f6b02432195afa3ab01b36d405",
+      clientID: "273381409494888",
+      clientSecret: "7752245834fc257678f9695c6684c29b",
       callbackURL: "http://ntwitter.nodejitsu.com/auth/facebook/callback"
     },
     github: {
       clientID: 'c2e0f478634366e1289d',
       clientSecret: '0bfde82383deeb99b28d0f6a9eac001a0deb798a',
       callbackURL: 'http://ntwitter.nodejitsu.com/auth/github/callback'
-    }
+    },
+    twitter: {
+      clientID: 'J3RJBEzrLd9lVloHf6uGQ',
+      clientSecret: 'DwAEaZX84iFg2LYdWHLHQF9l4idAqQWPL2LTCPnEjM',
+      callbackURL: "http://ntwitter.nodejitsu.com//auth/twitter/callback"
+    },
   },
   test: {
     db: 'mongodb://root:volvo76@ds039078.mongolab.com:39078/ntwitter',
@@ -25,15 +30,20 @@ module.exports = {
       name: 'Nodejs Express Mongoose Demo'
     },
     facebook: {
-      clientID: "680920985255957",
-      clientSecret: "2d5c19f6b02432195afa3ab01b36d405",
+      clientID: "273381409494888",
+      clientSecret: "7752245834fc257678f9695c6684c29b",
       callbackURL: "http://ntwitter.nodejitsu.com/auth/facebook/callback"
     },
     github: {
       clientID: 'c2e0f478634366e1289d',
       clientSecret: '0bfde82383deeb99b28d0f6a9eac001a0deb798a',
       callbackURL: 'http://ntwitter.nodejitsu.com/auth/github/callback'
-    }
+    },
+    twitter: {
+      clientID: 'J3RJBEzrLd9lVloHf6uGQ',
+      clientSecret: 'DwAEaZX84iFg2LYdWHLHQF9l4idAqQWPL2LTCPnEjM',
+      callbackURL: "http://ntwitter.nodejitsu.com//auth/twitter/callback"
+    },
   },
   production: {
     db: 'mongodb://root:volvo76@ds039078.mongolab.com:39078/ntwitter',
@@ -42,14 +52,19 @@ module.exports = {
       name: 'Nodejs Express Mongoose Demo'
     },
     facebook: {
-      clientID: "680920985255957",
-      clientSecret: "2d5c19f6b02432195afa3ab01b36d405",
+      clientID: "273381409494888",
+      clientSecret: "7752245834fc257678f9695c6684c29b",
       callbackURL: "http://ntwitter.nodejitsu.com/auth/facebook/callback"
     },
     github: {
       clientID: 'c2e0f478634366e1289d',
       clientSecret: '0bfde82383deeb99b28d0f6a9eac001a0deb798a',
       callbackURL: 'http://ntwitter.nodejitsu.com/auth/github/callback'
-    }
+    },
+    twitter: {
+      clientID: 'J3RJBEzrLd9lVloHf6uGQ',
+      clientSecret: 'DwAEaZX84iFg2LYdWHLHQF9l4idAqQWPL2LTCPnEjM',
+      callbackURL: "http://ntwitter.nodejitsu.com//auth/twitter/callback"
+    },
   }
 };
