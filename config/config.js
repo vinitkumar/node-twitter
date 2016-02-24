5134ebc2a672987fa9c93f65ca12d937932e2797
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2016-02-24 22:59:16 +0530

    try and fix login again

--- config/config.js
+++ config/config.js
@@ -1,5 +1,6 @@
-var path = require('path'),
-    rootPath = path.normalize(__dirname + '/..');
+var path = require('path');
+var rootPath = path.normalize(__dirname + '/..');
+
 module.exports = {
   development: {
     db: 'mongodb://root:volvo76@ds039078.mongolab.com:39078/ntwitter',
@@ -7,21 +8,11 @@ module.exports = {
     app: {
       name: 'Node Twitter'
     },
-    facebook: {
-      clientID: "1446247922298685",
-      clientSecret: "0bb080780a38bdbbdbe192f9e9e6579b",
-      callbackURL: "http://ntwitter.nodejitsu.com/auth/facebook/callback"
-    },
     github: {
       clientID: 'c2e0f478634366e1289d',
       clientSecret: '0bfde82383deeb99b28d0f6a9eac001a0deb798a',
-      callbackURL: 'http://ntwitter.nodejitsu.com/auth/github/callback'
-    },
-    twitter: {
-       clientID: 'FCx4C2yOFdkRaY7NXEw7bbe2S',
-       clientSecret: 'UVbi0ZNI0Fosglct06tGmuf740waDJ6ZrI5xPQIxarx2OnMSs2',
-       callbackURL: "http://ntwitter.nodejitsu.com/auth/twitter/callback"
-     },
+      callbackURL: 'http://nitter.herokuapp.com/auth/github/callback'
+    }
   },
   test: {
     db: 'mongodb://root:volvo76@ds039078.mongolab.com:39078/ntwitter',
@@ -29,21 +20,11 @@ module.exports = {
     app: {
       name: 'Nodejs Express Mongoose Demo'
     },
-    facebook: {
-      clientID: "273381409494888",
-      clientSecret: "7752245834fc257678f9695c6684c29b",
-      callbackURL: "http://ntwitter.nodejitsu.com/auth/facebook/callback"
-    },
     github: {
       clientID: 'c2e0f478634366e1289d',
       clientSecret: '0bfde82383deeb99b28d0f6a9eac001a0deb798a',
-      callbackURL: 'http://ntwitter.nodejitsu.com/auth/github/callback'
-    },
-     twitter: {
-       clientID: 'J3RJBEzrLd9lVloHf6uGQ',
-       clientSecret: 'DwAEaZX84iFg2LYdWHLHQF9l4idAqQWPL2LTCPnEjM',
-       callbackURL: "http://ntwitter.nodejitsu.com/auth/twitter/callback"
-     },
+      callbackURL: 'http://nitter.herokuapp.com/auth/github/callback'
+    }
   },
   production: {
     db: 'mongodb://root:volvo76@ds039078.mongolab.com:39078/ntwitter',
@@ -51,20 +32,10 @@ module.exports = {
     app: {
       name: 'Nodejs Express Mongoose Demo'
     },
-    facebook: {
-      clientID: "273381409494888",
-      clientSecret: "7752245834fc257678f9695c6684c29b",
-      callbackURL: "http://ntwitter.nodejitsu.com/auth/facebook/callback"
-    },
     github: {
       clientID: 'c2e0f478634366e1289d',
       clientSecret: '0bfde82383deeb99b28d0f6a9eac001a0deb798a',
-      callbackURL: 'http://ntwitter.nodejitsu.com/auth/github/callback'
-    },
-    twitter: {
-       clientID: 'J3RJBEzrLd9lVloHf6uGQ',
-       clientSecret: 'DwAEaZX84iFg2LYdWHLHQF9l4idAqQWPL2LTCPnEjM',
-       callbackURL: "http://ntwitter.nodejitsu.com/auth/twitter/callback"
-     },
+      callbackURL: 'http://nitter.herokuapp.com/auth/github/callback'
+    }
   }
 };
--- config/passport.js
+++ config/passport.js
@@ -1,10 +1,7 @@
 
 var mongoose = require('mongoose'),
     LocalStrategy = require('passport-local').Strategy,
-    TwitterStrategy = require('passport-twitter').Strategy,
-    FacebookStrategy = require('passport-facebook').Strategy,
     GitHubStrategy = require('passport-github').Strategy,
-    GoogleStrategy = require('passport-google-oauth').Strategy,
     User = mongoose.model('User');
 
 
@@ -42,35 +39,6 @@ module.exports = function (passport, config) {
   ));
 
 
-  // use facebook strategy
-  passport.use(new FacebookStrategy({
-        clientID: config.facebook.clientID,
-        clientSecret: config.facebook.clientSecret,
-        callbackURL: config.facebook.callbackURL
-    },
-    function(accessToken, refreshToken, profile, done) {
-      User.findOne({ 'facebook.id': profile.id }, function (err, user) {
-        if (err) { return done(err); }
-        if (!user) {
-          user = new User({
-              name: profile.displayName,
-              email: profile.emails[0].value,
-              username: profile.username,
-              provider: 'facebook',
-              facebook: profile._json
-          });
-          user.save(function (err) {
-            if (err) console.log(err);
-            return done(err, user);
-          });
-        }
-        else {
-          return done(err, user);
-        }
-      });
-    }
-  ));
-
   // use github strategy
   passport.use(new GitHubStrategy({
       clientID: config.github.clientID,
@@ -98,31 +66,5 @@ module.exports = function (passport, config) {
     }
   ));
 
-  passport.use(new TwitterStrategy({
-      consumerKey: config.twitter.clientID,
-      consumerSecret: config.twitter.clientSecret,
-      callbackURL: config.twitter.callbackURL
-    },
-    function(token, tokenSecret, profile, done) {
-      User.findOne({ 'twitter.id_str': profile.id }, function (err, user) {
-        if (err) { return done(err) }
-        if (!user) {
-          user = new User({
-            name: profile.displayName,
-            username: profile.username,
-            provider: 'twitter',
-            twitter: profile._json
-          })
-          user.save(function (err) {
-            if (err) console.log(err)
-            return done(err, user)
-          })
-        }
-        else {
-          return done(err, user)
-        }
-      })
-    }
-  ))
 
 };
