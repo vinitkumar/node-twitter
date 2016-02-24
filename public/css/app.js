81f55bd34056d13639f99f3dc7ca8272353b62f5
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2016-02-24 21:58:13 +0530

    first deploy to heroku

---
+++ public/css/app.js
@@ -0,0 +1,36 @@
+// function showdata(data) {
+//   $.each( data, function( key, val ) {
+//     console.log('key is '+ key + ' value is ' + val);
+//   });
+//   if (typeof(val) === 'object'){
+//     debugger;
+//     showdata(val);
+//   } 
+// }
+
+//called with every property and it's value
+function process(key,value) {
+    var items = [];
+    if (typeof(value !== 'object')) {
+     debugger;
+     items.push( "<li id='" + key + "'>" + value + "</li>" ); 
+    }
+      $( "<ul/>", {
+        "class": "data-list",
+        html: items.join( "" )
+      }).appendTo( ".container" );
+}
+
+function traverse(o,func) {
+    for (var i in o) {
+        func.apply(this,[i,o[i]]);  
+        if (o[i] !== null && typeof(o[i])=="object") {
+            //going on step down in the object tree!!
+            traverse(o[i],func);
+        }
+    }
+}
+
+$.getJSON( "facebook.json", function( data ) {
+  traverse(data, process);  
+});
