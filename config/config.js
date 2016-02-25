401a62e2a5cb2279d823b34d6890a1ac1cf85738
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2016-02-25 19:51:56 +0530

    update node.js on travis for good build

--- config/config.js
+++ config/config.js
@@ -9,9 +9,9 @@ module.exports = {
       name: 'Node Twitter'
     },
     github: {
-      clientID: 'c2e0f478634366e1289d',
-      clientSecret: '0bfde82383deeb99b28d0f6a9eac001a0deb798a',
-      callbackURL: 'http://nitter.herokuapp.com/auth/github/callback'
+      clientID: 'e3930cf94c772ba10ef1',
+      clientSecret: 'fb1284b1874444a9c0c55c963092f836596ecc56',
+      callbackURL: 'http://localhost:3000/auth/github/callback'
     }
   },
   test: {
