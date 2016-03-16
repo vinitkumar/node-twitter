f08def6b85e68cb3ae18e851ba767b73c4b488a6
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2016-03-17 00:55:29 +0530

    add todo

--- app/controllers/analytics.js
+++ app/controllers/analytics.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var Analytics = mongoose.model('Analytics');
 
 exports.index = function(req, res) {
+  // TODO: add pagination
   Analytics.list({}, function(err, analytics) {
     res.render('analytics/index', {
       title: 'List of users',
