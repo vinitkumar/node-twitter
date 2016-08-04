49f91af8dff8f20506228f509b62a1e8c6295780
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2016-08-04 23:15:29 +0530

    change from var to let/const

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-var express = require('express'),
-    fs = require('fs'),
-    passport = require('passport');
+const express = require('express');
+const fs = require('fs');
+const passport = require('passport');
 
-var env = process.env.NODE_ENV || 'development',
-   config = require('./config/config')[env],
-   auth = require('./config/middlewares/authorization'),
-   mongoose = require('mongoose');
+const env = process.env.NODE_ENV || 'development';
+const config = require('./config/config')[env];
+const auth = require('./config/middlewares/authorization');
+const mongoose = require('mongoose');
 
 mongoose.connect(config.db);
 
-var models_path = __dirname+'/app/models';
+const models_path = __dirname+'/app/models';
 fs.readdirSync(models_path).forEach(file => {
   require(models_path+'/'+file);
 });
 
 require('./config/passport')(passport, config);
 
-var app = express();
+const app = express();
 require('./config/express')(app, config, passport);
 require('./config/routes')(app, passport, auth);
-var port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port);
 console.log('Express app started on port '+port);
 exports = module.exports = app;
