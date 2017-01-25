993ea8cb15f1a595e847db7d43774224016c7509
tatitanizaki <tatiana.tanizaki@gmail.com>
2017-01-25 10:50:20 -0500

    Changed Tokens and IDs
    Changed:
    clientID
    clientSecret
    
    db path

--- config/config.js
+++ config/config.js
@@ -3,39 +3,39 @@ var rootPath = path.normalize(__dirname + '/..');
 
 module.exports = {
   development: {
-    db: 'mongodb://root:volvo76@ds039078.mongolab.com:39078/ntwitter',
+    db: 'mongodb://localhost:27017/test',
     root: rootPath,
     app: {
       name: 'Node Twitter'
     },
     github: {
-      clientID: 'e3930cf94c772ba10ef1',
-      clientSecret: 'fb1284b1874444a9c0c55c963092f836596ecc56',
+      clientID: '48f4a99ce717f5531ff2',
+      clientSecret: '26b13bec1117b421a5ca1b81b0fea8f77e59146e',
       callbackURL: 'http://localhost:3000/auth/github/callback'
     }
   },
   test: {
-    db: 'mongodb://root:volvo76@ds039078.mongolab.com:39078/ntwitter',
+    db: 'mongodb://localhost:27017/test',
     root: rootPath,
     app: {
       name: 'Nodejs Express Mongoose Demo'
     },
     github: {
-      clientID: 'c2e0f478634366e1289d',
-      clientSecret: '0bfde82383deeb99b28d0f6a9eac001a0deb798a',
-      callbackURL: 'http://nitter.herokuapp.com/auth/github/callback'
+      clientID: '48f4a99ce717f5531ff2',
+      clientSecret: '26b13bec1117b421a5ca1b81b0fea8f77e59146e',
+      callbackURL: 'http://tatwitter.herokuapp.com/auth/github/callback'
     }
   },
   production: {
-    db: 'mongodb://root:volvo76@ds039078.mongolab.com:39078/ntwitter',
+    db: 'mongodb://localhost:27017/test',
     root: rootPath,
     app: {
       name: 'Nodejs Express Mongoose Demo'
     },
     github: {
-      clientID: 'c2e0f478634366e1289d',
-      clientSecret: '0bfde82383deeb99b28d0f6a9eac001a0deb798a',
-      callbackURL: 'http://nitter.herokuapp.com/auth/github/callback'
+      clientID: '48f4a99ce717f5531ff2',
+      clientSecret: '26b13bec1117b421a5ca1b81b0fea8f77e59146e',
+      callbackURL: 'http://tatwitter.herokuapp.com/auth/github/callback'
     }
   }
 };
