d3b16069a9189a7a8c65f1f90ca88b12593cadee
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2017-09-03 01:37:45 +0530

    make sure ip is there

--- app/controllers/tweets.js
+++ app/controllers/tweets.js
@@ -6,21 +6,23 @@ const _ = require("underscore");
 
 function logAnalytics(req) {
   const url = req.protocol + "://" + req.get("host") + req.originalUrl;
-  const crudeIpArray = req.ip.split(":");
-  const ipArrayLength = crudeIpArray.length;
-  // cleanup IP to remove unwanted characters
-  const cleanIp = crudeIpArray[ipArrayLength - 1];
-  if (req.get("host").split(":")[0] !== "localhost") {
-    const analytics = new Analytics({
-      ip: cleanIp,
-      user: req.user,
-      url: url
-    });
-    analytics.save(err => {
-      if (err) {
-        console.log(err);
-      }
-    });
+  if (req.ip !== undefined) {
+    const crudeIpArray = req.ip.split(":");
+    const ipArrayLength = crudeIpArray.length;
+    // cleanup IP to remove unwanted characters
+    const cleanIp = crudeIpArray[ipArrayLength - 1];
+    if (req.get("host").split(":")[0] !== "localhost") {
+      const analytics = new Analytics({
+        ip: cleanIp,
+        user: req.user,
+        url: url
+      });
+      analytics.save(err => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    }
   }
 }
 
