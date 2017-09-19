81cd2609a3fac714d0f1c29663f8ef77dd5568b2
Robert Cooper <robert.cooper.qu@gmail.com>
2017-09-19 13:34:04 -0400

    Fixed bodyParser() deprecation warnings

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const passport = require('passport');
-
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config/config')[env];
 const auth = require('./config/middlewares/authorization');
 const mongoose = require('mongoose');
+const app = express();
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.db, {
@@ -14,16 +14,12 @@ mongoose.connect(config.db, {
   useMongoClient: true
 });
 
-// mongoose.connect(config.db);
-
 const models_path = __dirname+'/app/models';
 fs.readdirSync(models_path).forEach(file => {
   require(models_path+'/'+file);
 });
 
 require('./config/passport')(passport, config);
-
-const app = express();
 require('./config/express')(app, config, passport);
 require('./config/routes')(app, passport, auth);
 const port = process.env.PORT || 3000;
