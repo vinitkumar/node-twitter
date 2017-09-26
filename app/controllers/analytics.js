a16891380c929a66243a82748d0e99feaf362e12
Robert Cooper <robert.cooper.qu@gmail.com>
2017-09-25 20:56:21 -0400

    Added comments to help with readability of pagination code

--- app/controllers/analytics.js
+++ app/controllers/analytics.js
@@ -42,38 +42,48 @@ exports.index = (req, res) => {
 };
 
 function createPagination (req, pages, page) {
-  let params = qs.parse(url.parse(req.url).query)
-  let str = ''
-  let clas
+  let params = qs.parse(url.parse(req.url).query);
+  let str = '';
+  let pageNumberClass;
   let pageCutLow = page - 1;
   let pageCutHigh = page + 1;
+  // Show the Previous button only if you are on a page other than the first
   if (page > 1) {
     str += '<li class="no"><a href="?page='+(page-1)+'">Previous</a></li>';
   }
+  // Show all the pagination elements if there are less than 6 pages total
   if (pages < 6) {
     for (let p = 1; p <= pages; p++) {
-      params.page = p
-      clas = page == p ? "active" : "no"
-      let href = '?' + qs.stringify(params)
-      str += '<li class="'+clas+'"><a href="'+ href +'">'+ p +'</a></li>'
+      params.page = p;
+      pageNumberClass = page == p ? "active" : "no";
+      let href = '?' + qs.stringify(params);
+      str += '<li class="'+pageNumberClass+'"><a href="'+ href +'">'+ p +'</a></li>';
     }
-  } else {
+  }
+  // Use "..." to collapse pages outside of a certain range
+  else {
+    // Show the very first page followed by a "..." at the beginning of the
+    // pagination section (after the Previous button)
     if (page > 2) {
       str += '<li class="no"><a href="?page=1">1</a></li>';
       if (page > 3) {
           str += '<li class="out-of-range">...</li>';
       }
     }
+    // Determine how many pages to show after the current page index
     if (page === 1) {
       pageCutHigh += 2;
     } else if (page === 2) {
       pageCutHigh += 1;
     }
+    // Determine how many pages to show before the current page index
     if (page === pages) {
       pageCutLow -= 2;
     } else if (page === pages-1) {
       pageCutLow -= 1;
     }
+    // Output the indexes for pages that fall inside the range of pageCutLow
+    // and pageCutHigh
     for (let p = pageCutLow; p <= pageCutHigh; p++) {
       if (p === 0) {
         p += 1;
@@ -81,11 +91,13 @@ function createPagination (req, pages, page) {
       if (p > pages) {
         continue
       }
-      params.page = p
-      clas = page == p ? "active" : "no"
-      let href = '?' + qs.stringify(params)
-      str += '<li class="'+clas+'"><a href="'+ href +'">'+ p +'</a></li>'
+      params.page = p;
+      pageNumberClass = page == p ? "active" : "no";
+      let href = '?' + qs.stringify(params);
+      str += '<li class="'+pageNumberClass+'"><a href="'+ href +'">'+ p +'</a></li>';
     }
+    // Show the very last page preceded by a "..." at the end of the pagination
+    // section (before the Next button)
     if (page < pages-1) {
       if (page < pages-2) {
         str += '<li class="out-of-range">...</li>';
@@ -93,8 +105,10 @@ function createPagination (req, pages, page) {
       str += '<li class="no"><a href="?page='+pages+'">'+pages+'</a></li>';
     }
   }
+  // Show the Next button only if you are on a page other than the last
   if (page < pages) {
     str += '<li class="no"><a href="?page='+(page+1)+'">Next</a></li>';
   }
-  return str
+  // Return the pagination string to be outputted in the pug templates
+  return str;
 }
