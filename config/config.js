28fe0bc9e35cc411702cff444e282f60e178aed1
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2017-10-01 22:23:08 +0530

    remove keys from config

--- config/config.js
+++ config/config.js
@@ -1,5 +1,9 @@
 const path = require("path");
 const rootPath = path.normalize(__dirname + "/..");
+const DB = process.env.DB;
+const clientID = process.env.GITHUB_CLIENT_ID;
+const clientSecret = process.env.GITHUB_CLIENT_SECRET;
+
 
 module.exports = {
   development: {
@@ -27,14 +31,14 @@ module.exports = {
     }
   },
   production: {
-    db: "mongodb://root:volvo76@ds039078.mongolab.com:39078/ntwitter",
+    db: DB,
     root: rootPath,
     app: {
       name: "Nodejs Express Mongoose Demo"
     },
     github: {
-      clientID: "c2e0f478634366e1289d",
-      clientSecret: "0bfde82383deeb99b28d0f6a9eac001a0deb798a",
+      clientID: clientID,
+      clientSecret: clientSecret,
       callbackURL: "http://nitter.herokuapp.com/auth/github/callback"
     }
   }
