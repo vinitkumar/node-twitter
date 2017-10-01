82d37c8f72fdb666823f999216d0d8fe4bec8327
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2017-10-01 21:04:15 +0530

    enforce tweet and comment length to be 140

--- app/models/tweets.js
+++ app/models/tweets.js
@@ -9,11 +9,11 @@ const setTags = tags => tags.split(",");
 
 // Tweet Schema
 const TweetSchema = new Schema({
-  body: { type: String, default: "", trim: true },
+  body: { type: String, default: "", trim: true, maxlength: 140},
   user: { type: Schema.ObjectId, ref: "User" },
   comments: [
     {
-      body: { type: String, default: "" },
+      body: { type: String, default: "", maxlength: 140},
       user: { type: Schema.ObjectId, ref: "User" },
       commenterName: { type: String, default: "" },
       createdAt: { type: Date, default: Date.now }
