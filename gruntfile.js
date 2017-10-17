ecbf6258f5cdceaa9030d79628bdea385817f81b
Robert Cooper <robert.cooper.qu@gmail.com>
2017-10-17 16:18:54 -0400

    Setup grunt to compile Sass files

--- gruntfile.js
+++ gruntfile.js
@@ -1,30 +1,27 @@
-let _ = require('underscore');
-
 module.exports = function (grunt) {
 
-  grunt.loadNpmTasks('grunt-contrib');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-
+  // Project
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    concat: {
-      dist: {
-        src: ['public/js/jquery.min.js', 'public/bootstrap.min.js', 'public/js/app.js'],
-        dest: 'public/js/<%= pkg.name %>.add.js'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
-      },
-      dist: {
-        files: {
-          'public/js/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
-        }
-      }
-    }
+    sass: {
+			dist: {
+				files: {
+					'public/css/style.css' : 'app/styles/main.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		}
   });
 
+  // Load Grunt plugins
+  grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
+  // Register custom tasks to run from the terminal
+	grunt.registerTask('default',['watch']);
 };
