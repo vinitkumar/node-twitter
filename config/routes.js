38d37940dbf1d4016a899207ba223986a8d90dd2
Robert Cooper <robert.cooper.qu@gmail.com>
2017-10-20 10:16:35 -0400

    Cleaned up the routes file

--- config/routes.js
+++ config/routes.js
@@ -1,12 +1,26 @@
-let async = require("async");
-
 module.exports = (app, passport, auth) => {
   const users = require("../app/controllers/users");
+  const apiv1 = require("../app/controllers/apiv1");
+  const chat = require('../app/controllers/chat');
+  const analytics = require("../app/controllers/analytics");
+  const tweets = require("../app/controllers/tweets");
+  const comments = require("../app/controllers/comments");
+  const favorites = require("../app/controllers/favorites");
+  const follows = require("../app/controllers/follows");
+
+  /**
+   * Main routes
+   */
+  app.get("/", auth.requiresLogin, tweets.index);
   app.get("/login", users.login);
   app.get("/signup", users.signup);
   app.get("/logout", users.logout);
   app.post("/users", users.create);
   app.get("/userslist", users.list);
+
+  /**
+   * User routes
+   */
   app.post(
     "/users/sessions",
     passport.authenticate("local", {
@@ -18,7 +32,12 @@ module.exports = (app, passport, auth) => {
   app.get("/users/:userId", users.show);
   app.get("/users/:userId/followers", users.showFollowers);
   app.get("/users/:userId/following", users.showFollowing);
+  app.post("/users/:userId/follow", auth.requiresLogin, follows.follow);
+  app.param("userId", users.user);
 
+  /**
+   * Authentication routes
+   */
   app.get(
     "/auth/github",
     passport.authenticate("github", { failureRedirect: "/login" }),
@@ -31,34 +50,29 @@ module.exports = (app, passport, auth) => {
   );
 
   /**
-   * API related code
+   * API routes
    */
-  const apiv1 = require("../app/controllers/apiv1");
   app.get("/apiv1/tweets", apiv1.tweetList);
   app.get("/apiv1/users", apiv1.usersList);
 
   /**
-   * Chat related code
+   * Chat routes
    */
-
-  const chat = require('../app/controllers/chat');
   app.get('/chat', auth.requiresLogin, chat.index);
   app.get('/chat/:id', auth.requiresLogin, chat.show);
   app.get('/chat/get/:userid', auth.requiresLogin, chat.getChat);
   app.post('/chats', auth.requiresLogin, chat.create);
   /**
-  * Analytics related code
+  * Analytics routes
   */
-  const analytics = require("../app/controllers/analytics");
   app.get("/analytics", analytics.index);
 
-  app.param("userId", users.user);
-
-  //tweets routes
-  const tweets = require("../app/controllers/tweets");
+  /**
+   * Tweet routes
+   */
   app.get("/tweets", tweets.index);
-  app.post("/tweets", auth.requiresLogin, tweets.create);
   app.get("/tweets/:id", tweets.show);
+  app.post("/tweets", auth.requiresLogin, tweets.create);
   app.post(
     "/tweets/:id",
     auth.requiresLogin,
@@ -73,11 +87,9 @@ module.exports = (app, passport, auth) => {
   );
   app.param("id", tweets.tweet);
 
-  //home route
-  app.get("/", auth.requiresLogin, tweets.index);
-
-  //comment routes
-  const comments = require("../app/controllers/comments");
+  /**
+   * Comment routes
+   */
   app.post("/tweets/:id/comments", auth.requiresLogin, comments.create);
   app.get("/tweets/:id/comments", auth.requiresLogin, comments.create);
   app.del("/tweets/:id/comments", auth.requiresLogin, comments.destroy);
@@ -85,15 +97,6 @@ module.exports = (app, passport, auth) => {
   /**
    * Favorite routes
    */
-  const favorites = require("../app/controllers/favorites");
-
   app.post("/tweets/:id/favorites", auth.requiresLogin, favorites.create);
   app.del("/tweets/:id/favorites", auth.requiresLogin, favorites.destroy);
-
-  /**
-    * Follow
-    */
-  const follows = require("../app/controllers/follows");
-
-  app.post("/users/:userId/follow", auth.requiresLogin, follows.follow);
 };
