1833c6d66c5e19547e1fac43d39e9553d0415ed9
Robert Cooper <robert.cooper.qu@gmail.com>
2017-10-20 13:45:42 -0400

    Installed and applied required modules for Express 4

--- config/express.js
+++ config/express.js
@@ -2,10 +2,17 @@
  * Module dependencies
  */
 const express = require("express");
-const mongoStore = require("connect-mongo")(express);
+const session = require('express-session');
+const compression = require('compression');
+// const favicon = require('serve-favicon');
+const errorHandler = require('errorhandler');
+const mongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
 const helpers = require("view-helpers");
-const bodyParser  = require('body-parser');;
+const bodyParser  = require('body-parser');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 const Raven = require('raven');
 
 module.exports = (app, config, passport) => {
@@ -17,63 +24,58 @@ module.exports = (app, config, passport) => {
     app.use(Raven.errorHandler());
   }
   app.use(
-    express.compress({
+    compression({
       filter: function(req, res) {
         return /json|text|javascript|css/.test(res.getHeader("Content-Type"));
       },
       level: 9
     })
   );
-  app.use(express.favicon());
+  // app.use(favicon());
   app.use(express.static(config.root + "/public"));
 
   if (process.env.NODE_ENV !== "test") {
-    app.use(express.logger("dev"));
+    // Use morgan logger if not in a development environment
+    app.use(morgan("dev"));
   }
-  app.configure("development", () => {
-    app.use(express.errorHandler());
+
+  if (process.env.NODE_ENV === "development") {
+    app.use(errorHandler());
     app.locals.pretty = true;
-  });
+  }
 
   app.set("views", config.root + "/app/views");
   app.set("view engine", "pug");
-  app.configure(() => {
-    app.use(helpers(config.app.name));
-    app.use(express.cookieParser());
-    app.use(bodyParser.urlencoded({
-      extended: true
-    }));
-    app.use(bodyParser.json());
-    app.use(express.methodOverride());
-    app.use(
-      express.session({
-        secret: "noobjs",
-        store: new mongoStore({
-          url: config.db,
-          collection: "sessions"
-        })
+
+  app.use(helpers(config.app.name));
+  app.use(cookieParser());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+  app.use(
+    session({
+      secret: "noobjs",
+      resave: false,
+      saveUninitialized: false,
+      store: new mongoStore({
+        url: config.db,
+        collection: "sessions"
       })
-    );
+    })
+  );
 
-    app.use(flash());
-    app.use(passport.initialize());
-    app.use(passport.session());
-    app.use(app.router);
+  app.use(flash());
+  app.use(passport.initialize());
+  app.use(passport.session());
 
-    app.use((err, req, res, next) => {
-      if (err.message.indexOf("not found") !== -1) {
-        return next();
-      }
-      console.log(err.stack);
+  app.use((err, req, res, next) => {
+    if (err.message.indexOf("not found") !== -1) {
+      return next();
+    }
+    console.log(err.stack);
 
-      res.status(500).render("pages/500", { error: err.stack });
-    });
-
-    app.use((req, res) => {
-      res.status(404).render("pages/404", {
-        url: req.originalUrl,
-        error: "Not found"
-      });
-    });
+    res.status(500).render("pages/500", { error: err.stack });
   });
 };
