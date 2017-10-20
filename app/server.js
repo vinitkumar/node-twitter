f94859e794e2e91129fd53173465915a00b98618
Robert Cooper <robert.cooper.qu@gmail.com>
2017-10-20 13:50:30 -0400

    Moved server.js into the app folder for better organization

--- app/server.js
+++ app/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const fs = require('fs');
 const passport = require('passport');
 const env = process.env.NODE_ENV || 'development';
-const config = require('./config/config')[env];
-const auth = require('./config/middlewares/authorization');
+const config = require('../config/config')[env];
+const auth = require('../config/middlewares/authorization');
 const mongoose = require('mongoose');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.db, {
@@ -15,14 +15,14 @@ mongoose.connect(config.db, {
   useMongoClient: true
 });
 
-const models_path = __dirname+'/app/models';
+const models_path = __dirname+'/models';
 fs.readdirSync(models_path).forEach(file => {
   require(models_path+'/'+file);
 });
 
-require('./config/passport')(passport, config);
-require('./config/express')(app, config, passport);
-require('./config/routes')(app, passport, auth);
+require('../config/passport')(passport, config);
+require('../config/express')(app, config, passport);
+require('../config/routes')(app, passport, auth);
 
 app.listen(port);
 console.log('Express app started on port ' + port);
