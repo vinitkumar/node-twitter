bb7740c3c272932bf57c50af29cc03f9ad2ddfcf
Robert Cooper <robert.cooper.qu@gmail.com>
2017-10-21 22:45:54 -0400

    Rearranged routes to get authentication middleware working

--- config/routes.js
+++ config/routes.js
@@ -1,48 +1,34 @@
+const express = require('express');
+const router = express.Router();
+
+const users = require("../app/controllers/users");
+const apiv1 = require("../app/controllers/apiv1");
+const chat = require('../app/controllers/chat');
+const analytics = require("../app/controllers/analytics");
+const tweets = require("../app/controllers/tweets");
+const comments = require("../app/controllers/comments");
+const favorites = require("../app/controllers/favorites");
+const follows = require("../app/controllers/follows");
+
 module.exports = (app, passport, auth) => {
-  const users = require("../app/controllers/users");
-  const apiv1 = require("../app/controllers/apiv1");
-  const chat = require('../app/controllers/chat');
-  const analytics = require("../app/controllers/analytics");
-  const tweets = require("../app/controllers/tweets");
-  const comments = require("../app/controllers/comments");
-  const favorites = require("../app/controllers/favorites");
-  const follows = require("../app/controllers/follows");
 
+  app.use("/", router);
   /**
-   * Main routes
+   * Main unauthenticated routes
    */
-  app.get("/", auth.requiresLogin, tweets.index);
-  app.get("/login", users.login);
-  app.get("/signup", users.signup);
-  app.get("/logout", users.logout);
-
-  /**
-   * User routes
-   */
-  app.get("/users/:userId", users.show);
-  app.get("/users/:userId/followers", users.showFollowers);
-  app.get("/users/:userId/following", users.showFollowing);
-  app.post("/users", users.create);
-  app.post(
-    "/users/sessions",
-    passport.authenticate("local", {
-      failureRedirect: "/login",
-      failureFlash: "Invalid email or password"
-    }),
-    users.session
-  );
-  app.post("/users/:userId/follow", auth.requiresLogin, follows.follow);
-  app.param("userId", users.user);
+  router.get("/login", users.login);
+  router.get("/signup", users.signup);
+  router.get("/logout", users.logout);
 
   /**
    * Authentication routes
    */
-  app.get(
+  router.get(
     "/auth/github",
     passport.authenticate("github", { failureRedirect: "/login" }),
     users.signin
   );
-  app.get(
+  router.get(
     "/auth/github/callback",
     passport.authenticate("github", { failureRedirect: "/login" }),
     users.authCallback
@@ -51,53 +37,83 @@ module.exports = (app, passport, auth) => {
   /**
    * API routes
    */
-  app.get("/apiv1/tweets", apiv1.tweetList);
-  app.get("/apiv1/users", apiv1.usersList);
+  router.get("/apiv1/tweets", apiv1.tweetList);
+  router.get("/apiv1/users", apiv1.usersList);
+
+  /**
+   * Authentication middleware
+   * All routes specified after this middleware require authentication in order
+   * to access
+   */
+  router.use(auth.requiresLogin)
+
+  /**
+   * Home route
+   */
+  router.get("/", tweets.index);
+  /**
+   * User routes
+   */
+  router.get("/users/:userId", users.show);
+  router.get("/users/:userId/followers", users.showFollowers);
+  router.get("/users/:userId/following", users.showFollowing);
+  router.post("/users", users.create);
+  router.post(
+    "/users/sessions",
+    passport.authenticate("local",
+    {
+      failureRedirect: "/login",
+      failureFlash: "Invalid email or password"
+    }),
+    users.session
+  );
+  router.post("/users/:userId/follow", follows.follow);
+  router.param("userId", users.user);
 
   /**
    * Chat routes
    */
-  app.get('/chat', auth.requiresLogin, chat.index);
-  app.get('/chat/:id', auth.requiresLogin, chat.show);
-  app.get('/chat/get/:userid', auth.requiresLogin, chat.getChat);
-  app.post('/chats', auth.requiresLogin, chat.create);
+  router.get('/chat', chat.index);
+  router.get('/chat/:id', chat.show);
+  router.get('/chat/get/:userid', chat.getChat);
+  router.post('/chats', chat.create);
   /**
   * Analytics routes
   */
-  app.get("/analytics", analytics.index);
+  router.get("/analytics", analytics.index);
 
   /**
    * Tweet routes
    */
-  app.route("/tweets")
+  router.route("/tweets")
     .get(tweets.index)
-    .post(auth.requiresLogin, tweets.create)
+    .post(tweets.create)
 
-  app.route("/tweets/:id")
-    .post(auth.requiresLogin, auth.tweet.hasAuthorization, tweets.update)
-    .delete(auth.requiresLogin, auth.tweet.hasAuthorization, tweets.destroy)
+  router.route("/tweets/:id")
+    .post(auth.tweet.hasAuthorization, tweets.update)
+    .delete(auth.tweet.hasAuthorization, tweets.destroy)
 
-  app.param("id", tweets.tweet);
+  router.param("id", tweets.tweet);
 
   /**
    * Comment routes
    */
-  app.route("/tweets/:id/comments")
-    .get(auth.requiresLogin, comments.create)
-    .post(auth.requiresLogin, comments.create)
-    .delete(auth.requiresLogin, comments.destroy)
+  router.route("/tweets/:id/comments")
+    .get(comments.create)
+    .post(comments.create)
+    .delete(comments.destroy)
 
   /**
    * Favorite routes
    */
-  app.route("/tweets/:id/favorites")
-    .post(auth.requiresLogin, favorites.create)
-    .delete(auth.requiresLogin, favorites.destroy)
+  router.route("/tweets/:id/favorites")
+    .post(favorites.create)
+    .delete(favorites.destroy)
 
   /**
    * Page not found route (must be at the end of all routes)
    */
-  app.use((req, res) => {
+  router.use((req, res) => {
     res.status(404).render("pages/404", {
       url: req.originalUrl,
       error: "Not found"
