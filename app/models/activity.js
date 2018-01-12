c6321399c8dc2b37a0d8fa66dd999eedc92c3fb2
Vinit Kumar <vinit1414.08@bitmesra.ac.in>
2018-01-12 12:24:05 +0530

    add model for notification and activity

---
+++ app/models/activity.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const Activity = new Schema({
+    activityStream: {type: String, default: "", maxlength: 400},
+    sender: { type: Schema.ObjectId, ref: "User"},
+    receiver: { type: Schema.ObjectId, ref: "User"},
+    createdAt: { type: Date, default: Date.now },
+});
+
+mongoose.model("Activity", Activity);
