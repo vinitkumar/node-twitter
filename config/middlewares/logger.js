03b2f2ec2575a6a9ceb9fe16fea223073222e6fd
Vinit Kumar <vinit.kumar@socialschools.nl>
2018-05-05 03:41:16 +0530

    related to #215, issue with null analytics on fresh install

--- config/middlewares/logger.js
+++ config/middlewares/logger.js
@@ -12,21 +12,23 @@ exports.analytics = (req, res, next) => {
     const cleanIp = crudeIpArray[ipArrayLength - 1];
     Analytics.findOne({ user: req.user}).sort({ createdAt: -1 }).exec(function (err, analytics) {
       let date = new Date();
-      if (new Date(analytics.createdAt).getDate() !== date.getDate()) {
-        if (req.get('host').split(':')[0] !== 'localhost') {
-          const analytics = new Analytics({
-            ip: cleanIp,
-            user: req.user,
-            url: url
-          });
-          analytics.save(err => {
-            if (err) {
-              logger.log(err);
-            }
-          });
+      if (analytics !== null) {
+        if (new Date(analytics.createdAt).getDate() !== date.getDate()) {
+          if (req.get('host').split(':')[0] !== 'localhost') {
+            const analytics = new Analytics({
+              ip: cleanIp,
+              user: req.user,
+              url: url
+            });
+            analytics.save(err => {
+              if (err) {
+                logger.log(err);
+              }
+            });
+          }
+        } else {
+          logger.log('Not creating a new analytics entry on the same day');
         }
-      } else {
-        logger.log('Not creating a new analytics entry on the same day');
       }
     });
   }
