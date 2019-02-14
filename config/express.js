d49d695a5e0efb48f78534fd3fb85f66bfb05338
Vinit Kumar <mail@vinitkumar.me>
2019-02-14 15:34:25 +0530

    chore: run prettier on the code

--- config/express.js
+++ config/express.js
@@ -1,15 +1,15 @@
 /**
  * Module dependencies
  */
-const express = require("express");
+const express = require('express');
 const session = require('express-session');
 const compression = require('compression');
 // const favicon = require('serve-favicon');
 const errorHandler = require('errorhandler');
-const mongoStore = require("connect-mongo")(session);
-const flash = require("connect-flash");
-const helpers = require("view-helpers");
-const bodyParser  = require('body-parser');
+const mongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
+const helpers = require('view-helpers');
+const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const cookieParser = require('cookie-parser');
 const Raven = require('raven');
@@ -17,23 +17,28 @@ const moment = require('moment');
 const morgan = require('morgan');
 
 module.exports = (app, config, passport) => {
-  app.set("showStackError", true);
+  app.set('showStackError', true);
   app.locals.moment = moment;
 
   // use morgan for logging
-  app.use(morgan('dev', {
-    skip: function (req, res) {
-      return res.statusCode < 400;
-    }, stream: process.stderr
-  }));
-
+  app.use(
+    morgan('dev', {
+      skip: function(req, res) {
+        return res.statusCode < 400;
+      },
+      stream: process.stderr,
+    }),
+  );
 
   // use morgan for logging
-  app.use(morgan('dev', {
-    skip: function (req, res) {
-      return res.statusCode >= 400
-    }, stream: process.stdout
-  }));
+  app.use(
+    morgan('dev', {
+      skip: function(req, res) {
+        return res.statusCode >= 400;
+      },
+      stream: process.stdout,
+    }),
+  );
   // setup Sentry to get any crashes
   if (process.env.SENTRY_DSN !== null) {
     Raven.config(process.env.SENTRY_DSN).install();
@@ -43,40 +48,41 @@ module.exports = (app, config, passport) => {
   app.use(
     compression({
       filter: function(req, res) {
-        return /json|text|javascript|css/.test(res.getHeader("Content-Type"));
+        return /json|text|javascript|css/.test(res.getHeader('Content-Type'));
       },
-      level: 9
-    })
+      level: 9,
+    }),
   );
   // app.use(favicon());
-  app.use(express.static(config.root + "/public"));
+  app.use(express.static(config.root + '/public'));
 
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV === 'development') {
     app.use(errorHandler());
     app.locals.pretty = true;
   }
 
-  app.set("views", config.root + "/app/views");
-  app.set("view engine", "pug");
-
+  app.set('views', config.root + '/app/views');
+  app.set('view engine', 'pug');
 
   app.use(helpers(config.app.name));
   app.use(cookieParser());
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    }),
+  );
   app.use(bodyParser.json());
   app.use(methodOverride('_method'));
   app.use(
     session({
-      secret: "noobjs",
+      secret: 'noobjs',
       resave: false,
       saveUninitialized: false,
       store: new mongoStore({
         url: config.db,
-        collection: "sessions"
-      })
-    })
+        collection: 'sessions',
+      }),
+    }),
   );
 
   app.use(flash());
@@ -84,11 +90,11 @@ module.exports = (app, config, passport) => {
   app.use(passport.session());
 
   app.use((err, req, res, next) => {
-    if (err.message.indexOf("not found") !== -1) {
+    if (err.message.indexOf('not found') !== -1) {
       return next();
     }
     console.log(err.stack);
 
-    res.status(500).render("pages/500", { error: err.stack });
+    res.status(500).render('pages/500', {error: err.stack});
   });
 };
