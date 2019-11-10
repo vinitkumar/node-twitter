19580a97be373320cc19f9aee7ea214e453d137c
irsol <irinywka@gmail.com>
2019-11-10 12:31:05 +0100

    Secrets and passwords moved to env file.

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,6 @@ WORKDIR /usr/src/app
 # Install app dependencies
 COPY package*.json ./
 RUN npm install
-ENV SECRET='abadjadjadja1223232412424'
 # Copy app source code
 COPY . .
 #Expose port and start application
--- config/config.js
+++ config/config.js
@@ -1,19 +1,25 @@
 const path = require("path");
 const rootPath = path.normalize(__dirname + "/..");
 const DB = process.env.DB;
-const clientID = process.env.GITHUB_CLIENT_ID;
-const clientSecret = process.env.GITHUB_CLIENT_SECRET;
+
+// Path to the file where environment variables
+const dotenv = require("dotenv");
+dotenv.config({path: ".env" });
 
 module.exports = {
   development: {
     db: "mongodb://localhost/ntwitter",
+    port: process.env.PORT,
     root: rootPath,
     app: {
       name: "Node Twitter"
     },
     github: {
-      clientID: "e3930cf94c772ba10ef1",
-      clientSecret: "fb1284b1874444a9c0c55c963092f836596ecc56",
+      // GITHUB_CLIENT_SECRET and GITHUB_CLIENT_ID should be defined in .env file
+      // which is stored locally on your computer or those variables values 
+      // can be passed from Docker container
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientID: process.env.GITHUB_CLIENT_ID,
       callbackURL: "http://localhost:3000/auth/github/callback"
     }
   },
@@ -24,8 +30,10 @@ module.exports = {
       name: "Nodejs Express Mongoose Demo"
     },
     github: {
-      clientID: "c2e0f478634366e1289d",
-      clientSecret: "0bfde82383deeb99b28d0f6a9eac001a0deb798a",
+      clientSecret: "process.env.GITHUB_CLIENT_SECRET",
+      clientID: "process.env.GITHUB_CLIENT_ID",
+      // clientID: "c2e0f478634366e1289d",
+      // clientSecret: "0bfde82383deeb99b28d0f6a9eac001a0deb798a",
       callbackURL: "http://localhost:3000/auth/github/callback"
     }
   },
@@ -36,8 +44,10 @@ module.exports = {
       name: "Nodejs Express Mongoose Demo"
     },
     github: {
-      clientID: clientID,
-      clientSecret: clientSecret,
+      clientID: "process.env.GITHUB_CLIENT_SECRET",
+      clientSecret: "process.env.GITHUB_CLIENT_ID",
+      // clientID: clientID,
+      // clientSecret: clientSecret,
       callbackURL: "http://nitter.herokuapp.com/auth/github/callback"
     }
   }
