bb0f1b700bea2cd4542041c58df9028ee92d8269
Vinit <mail@vinitkumar.me>
2020-10-31 22:48:59 +0530

    feat: upgrade deps and improve an error message
    Authored-by: Vinit Kumar <mail@vinitkumar.me>
    Signed-off-by: Vinit Kumar <mail@vinitkumar.me>

--- app/middlewares/logger.js
+++ app/middlewares/logger.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 
 const level = process.env.LOG_LEVEL || 'debug';
 
-const logger = new winston.Logger({
+const logger = new winston.createLogger({
   transports: [
     new winston.transports.Console({
       level: level,
