f82b2db45d6e1f4139917b3a90b73fc2b0c12341
Vinit Kumar <vinit.kumar@promobitech.com>
2026-01-02 14:21:45 +0530

    Fix: Add missing context variables to all user controller render calls
    - Login page now shows tweet/user/analytics counts
    - Followers/Following pages now properly populate and pass user lists
    - Profile card now receives all required count variables
    - Delete user now actually deletes the user and their tweets
    - User load now includes followers/following fields

--- app/controllers/users.ts
+++ app/controllers/users.ts
@@ -8,7 +8,10 @@ export const user = async (req: Request, res: Response, next: any, id: string) =
   const User = mongoose.model('User');
 
   try {
-    const user = await (User as any).load({ criteria: { _id: id } });
+    const user = await (User as any).load({ 
+      criteria: { _id: id },
+      select: 'name username github followers following'
+    });
     if (!user) return next(new Error('Failed to load User ' + id));
     (req as any).profile = user;
     next();
@@ -33,9 +36,19 @@ export const create = async (req: Request, res: Response) => {
       return res.redirect('/users/' + user._id);
     });
   } catch (err: any) {
+    const Tweet = mongoose.model('Tweet');
+    const Analytics = mongoose.model('Analytics');
+    
+    const tweetCount = await Tweet.countDocuments();
+    const userCount = await User.countDocuments();
+    const analyticsCount = await Analytics.countDocuments();
+    
     return res.render('pages/login', {
       errors: err.errors,
-      user: user
+      user: user,
+      tweetCount: tweetCount,
+      userCount: userCount,
+      analyticsCount: analyticsCount
     });
   }
 };
@@ -88,23 +101,83 @@ export const show = async (req: Request, res: Response) => {
 /**
  * Show followers
  */
-export const showFollowers = (req: Request, res: Response) => {
+export const showFollowers = async (req: Request, res: Response) => {
+  const User = mongoose.model('User');
+  const Tweet = mongoose.model('Tweet');
   const user = (req as any).profile;
-  res.render('pages/followers', {
-    title: user.name + ' followers',
-    user: user
-  });
+
+  try {
+    // Populate followers
+    const populatedUser = await User.findById(user._id)
+      .populate('followers', 'name username github')
+      .exec();
+    
+    const followers = populatedUser?.followers || [];
+    
+    // Get counts for profile card
+    const tweetCount = await Tweet.countDocuments({ user: user._id });
+    const followerCount = user.followers ? user.followers.length : 0;
+    const followingCount = user.following ? user.following.length : 0;
+    
+    res.render('pages/followers', {
+      title: user.name + ' followers',
+      user: user,
+      followers: followers,
+      tweetCount: tweetCount,
+      followerCount: followerCount,
+      followingCount: followingCount
+    });
+  } catch (err) {
+    res.render('pages/followers', {
+      title: user.name + ' followers',
+      user: user,
+      followers: [],
+      tweetCount: 0,
+      followerCount: 0,
+      followingCount: 0
+    });
+  }
 };
 
 /**
  * Show following
  */
-export const showFollowing = (req: Request, res: Response) => {
+export const showFollowing = async (req: Request, res: Response) => {
+  const User = mongoose.model('User');
+  const Tweet = mongoose.model('Tweet');
   const user = (req as any).profile;
-  res.render('pages/followers', {
-    title: user.name + ' following',
-    user: user
-  });
+
+  try {
+    // Populate following
+    const populatedUser = await User.findById(user._id)
+      .populate('following', 'name username github')
+      .exec();
+    
+    const following = populatedUser?.following || [];
+    
+    // Get counts for profile card
+    const tweetCount = await Tweet.countDocuments({ user: user._id });
+    const followerCount = user.followers ? user.followers.length : 0;
+    const followingCount = user.following ? user.following.length : 0;
+    
+    res.render('pages/followers', {
+      title: user.name + ' following',
+      user: user,
+      followers: following, // Using 'followers' template variable for both
+      tweetCount: tweetCount,
+      followerCount: followerCount,
+      followingCount: followingCount
+    });
+  } catch (err) {
+    res.render('pages/followers', {
+      title: user.name + ' following',
+      user: user,
+      followers: [],
+      tweetCount: 0,
+      followerCount: 0,
+      followingCount: 0
+    });
+  }
 };
 
 /**
@@ -119,10 +192,23 @@ export const update = (req: Request, res: Response) => {
 /**
  * Delete user
  */
-export const deleteUser = (req: Request, res: Response) => {
+export const deleteUser = async (req: Request, res: Response) => {
+  const User = mongoose.model('User');
+  const Tweet = mongoose.model('Tweet');
   const user = (req as any).profile;
-  // Implement delete logic
-  res.redirect('/');
+  
+  try {
+    // Delete user's tweets
+    await Tweet.deleteMany({ user: user._id });
+    // Delete the user
+    await User.findByIdAndDelete(user._id);
+    // Logout and redirect
+    req.logout((err: any) => {
+      res.redirect('/login');
+    });
+  } catch (err) {
+    res.redirect('/');
+  }
 };
 
 /**
@@ -135,10 +221,30 @@ export const session = (req: Request, res: Response) => {
 /**
  * Login page
  */
-export const login = (req: Request, res: Response) => {
-  res.render('pages/login', {
-    title: 'Login'
-  });
+export const login = async (req: Request, res: Response) => {
+  const Tweet = mongoose.model('Tweet');
+  const User = mongoose.model('User');
+  const Analytics = mongoose.model('Analytics');
+
+  try {
+    const tweetCount = await Tweet.countDocuments();
+    const userCount = await User.countDocuments();
+    const analyticsCount = await Analytics.countDocuments();
+
+    res.render('pages/login', {
+      title: 'Login',
+      tweetCount: tweetCount,
+      userCount: userCount,
+      analyticsCount: analyticsCount
+    });
+  } catch (err) {
+    res.render('pages/login', {
+      title: 'Login',
+      tweetCount: 0,
+      userCount: 0,
+      analyticsCount: 0
+    });
+  }
 };
 
 /**
